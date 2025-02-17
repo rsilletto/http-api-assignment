43aa8da1dc908219b7fdc5c2e1c678a65a657be1
Rose Silletto <ras5491@rit.edu>
2025-02-17 03:41:02 -0500

    start

---
+++ src/htmlResponses.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+// const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+const styles = fs.readFileSync(`${__dirname}/../client/style.css`);
+
+const getIndex = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const getStyles = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(styles);
+  response.end();
+};
+
+module.exports.getIndex = getIndex;
+module.exports.getStyles = getStyles;
---
+++ src/server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+// const textHandler = require('./textResponses.js');
+// const jsonHandler = require('./jsonResponses.js');
+// const imageHandler = require('./imageResponses.js');
+// const mediaHandler = require('./mediaResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/style.css':
+      htmlHandler.getStyles(request, response);
+      break;
+    case '/page2':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/page3':
+      htmlHandler.getIndex(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.01:${port}`);
+});
